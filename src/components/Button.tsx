b825a8cdb76cd71c3efc6656a77eb5e9d1baf47d
Gabriele Di Grazia <gabrii539@gmail.com>
2024-01-30 22:16:32 +0100

    feat: added Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+    background-color: #5cd5f9;
+    border: none;
+    border-radius: 8px;
+    padding: .3em 1em;
+`
+
+export default Button
